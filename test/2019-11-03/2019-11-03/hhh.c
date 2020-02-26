a9020fab59669428682fc113fc64aa1a3c55592b
wolfGuidao <2351903911@qq.com>
2020-02-26 08:46:42 +0800

    remove lastyear lianxi

--- test/2019-11-03/2019-11-03/hhh.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int arr[100] = "";
-	gets(arr);
-}
--- test/编程练习09-15/编程练习09-15/源1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char a = 'a';
-	putchar(a);
-	system("pause");
-	return 0;
-}
